b0ca7c1adcfd1d91507aa8958bd9e97c33480014
Rinat Salekhov <staffordbridgestudio@gmail.com>
2024-01-23 15:04:04 +0300

    change 23.1.2024

--- Genre.sql
+++ Genre.sql
@@ -1,55 +1,42 @@
-CREATE TABLE IF NOT EXISTS Genre (
-    genre_id INT PRIMARY KEY,
-    genre_name VARCHAR(255) NOT NULL
+CREATE TABLE Genres (
+    GenreID INT PRIMARY KEY,
+    GenreName VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Artist (
-    artist_id INT PRIMARY KEY,
-    artist_name VARCHAR(255) NOT NULL
+CREATE TABLE Artists (
+    ArtistID INT PRIMARY KEY,
+    ArtistName VARCHAR(255) NOT NULL,
+    GenreID INT,
+    FOREIGN KEY (GenreID) REFERENCES Genres(GenreID)
 );
 
-CREATE TABLE IF NOT EXISTS ArtistGenre (
-    artist_id INT,
-    genre_id INT,
-    PRIMARY KEY (artist_id, genre_id),
-    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
-    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+CREATE TABLE Albums (
+    AlbumID INT PRIMARY KEY,
+    AlbumName VARCHAR(255) NOT NULL,
+    ReleaseYear INT,
+    ArtistID INT,
+    FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID)
 );
 
-CREATE TABLE IF NOT EXISTS Album (
-    album_id INT PRIMARY KEY,
-    album_name VARCHAR(255) NOT NULL,
-    release_year INT NOT NULL,
-    artist_id INT,
-    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+CREATE TABLE Tracks (
+    TrackID INT PRIMARY KEY,
+    TrackName VARCHAR(255) NOT NULL,
+    Duration INT,
+    AlbumID INT,
+    FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID)
 );
 
-CREATE TABLE IF NOT EXISTS ArtistAlbum (
-    artist_id INT,
-    album_id INT,
-    PRIMARY KEY (artist_id, album_id),
-    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
-    FOREIGN KEY (album_id) REFERENCES Album(album_id)
+CREATE TABLE Compilations (
+    CompilationID INT PRIMARY KEY,
+    CompilationName VARCHAR(255) NOT NULL,
+    ReleaseYear INT
 );
 
-CREATE TABLE IF NOT EXISTS Track (
-    track_id INT PRIMARY KEY,
-    track_name VARCHAR(255) NOT NULL,
-    duration INT NOT NULL,
-    album_id INT,
-    FOREIGN KEY (album_id) REFERENCES Album(album_id)
+CREATE TABLE CompilationTracks (
+    CompilationID INT,
+    TrackID INT,
+    PRIMARY KEY (CompilationID, TrackID),
+    FOREIGN KEY (CompilationID) REFERENCES Compilations(CompilationID),
+    FOREIGN KEY (TrackID) REFERENCES Tracks(TrackID)
 );
 
-CREATE TABLE IF NOT EXISTS Compilation (
-    compilation_id INT PRIMARY KEY,
-    compilation_name VARCHAR(255) NOT NULL,
-    release_year INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS CompilationTrack (
-    compilation_id INT,
-    track_id INT,
-    PRIMARY KEY (compilation_id, track_id),
-    FOREIGN KEY (compilation_id) REFERENCES Compilation(compilation_id),
-    FOREIGN KEY (track_id) REFERENCES Track(track_id)
-);
