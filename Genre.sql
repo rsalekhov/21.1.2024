f73c274b32e717a9589d07ebe6c29454f61b6a23
Rinat Salekhov <staffordbridgestudio@gmail.com>
2024-01-25 14:47:33 +0300

    25.1.2024

--- Genre.sql
+++ Genre.sql
@@ -1,35 +1,31 @@
 CREATE TABLE Genres (
     GenreID INT PRIMARY KEY,
-    GenreName VARCHAR(255) NOT NULL
+    GenreName VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE Artists (
     ArtistID INT PRIMARY KEY,
-    ArtistName VARCHAR(255) NOT NULL,
-    GenreID INT,
-    FOREIGN KEY (GenreID) REFERENCES Genres(GenreID)
+    ArtistName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Albums (
     AlbumID INT PRIMARY KEY,
     AlbumName VARCHAR(255) NOT NULL,
-    ReleaseYear INT,
-    ArtistID INT,
-    FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID)
+    ReleaseYear INT CHECK (ReleaseYear >= 1900 AND ReleaseYear <= 2100)
 );
 
 CREATE TABLE Tracks (
     TrackID INT PRIMARY KEY,
     TrackName VARCHAR(255) NOT NULL,
-    Duration INT,
+    Duration INT CHECK (Duration >= 0),
     AlbumID INT,
     FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID)
 );
 
 CREATE TABLE Compilations (
     CompilationID INT PRIMARY KEY,
-    CompilationName VARCHAR(255) NOT NULL,
-    ReleaseYear INT
+    CompilationName VARCHAR(255) UNIQUE NOT NULL,
+    ReleaseYear INT CHECK (ReleaseYear >= 1900 AND ReleaseYear <= 2100)
 );
 
 CREATE TABLE CompilationTracks (
